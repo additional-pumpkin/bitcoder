e2d6550b5f1e47e12255353c73fb4e4298b95e14
additional-pumpkin <additional.pumpkin@gmail.com>
2023-11-04 12:51:35 +0200

    parse torrent files

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,98 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Context;
+use clap::{Parser, Subcommand};
+
+use serde::de::{self, Visitor};
+use serde::Deserialize;
+use serde_bencode;
+
+use std::fmt;
+use std::path::PathBuf;
+
+#[derive(Debug, Clone, Deserialize)]
+struct Torrent {
+    announce: String,
+    info: Info,
+}
+#[derive(Debug, Clone)]
+struct Hashes(Vec<[u8; 20]>);
+struct HashesVisitor;
+
+impl<'de> Visitor<'de> for HashesVisitor {
+    type Value = Hashes;
+
+    fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+        formatter.write_str("a byte string whose length is a multiple of 20")
+    }
+
+    fn visit_bytes<E>(self, v: &[u8]) -> Result<Self::Value, E>
+    where
+        E: de::Error,
+    {
+        if v.len() % 20 != 0 {
+            return Err(E::custom(format!("length is {}", v.len())));
+        }
+        Ok(Hashes(
+            v.chunks_exact(20)
+                .map(|slice_20| slice_20.try_into().expect("guaranteed to be length 20"))
+                .collect(),
+        ))
+    }
+}
+
+impl<'de> Deserialize<'de> for Hashes {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        deserializer.deserialize_bytes(HashesVisitor)
+    }
+}
+
+#[derive(Debug, Clone, Deserialize)]
+struct Info {
+    name: String,
+    #[serde(rename = "piece length")]
+    piece_length: usize,
+    pieces: Hashes,
+    #[serde(flatten)]
+    keys: Keys,
+}
+#[derive(Debug, Clone, Deserialize)]
+#[serde(untagged)]
+enum Keys {
+    SingleFile { length: usize },
+    MultiFile { files: File },
+}
+#[derive(Debug, Clone, Deserialize)]
+
+struct File {
+    length: usize,
+    path: Vec<String>,
+}
+#[derive(Parser, Debug)]
+struct Args {
+    #[command(subcommand)]
+    command: Command,
+}
+
+#[derive(Subcommand, Debug)]
+enum Command {
+    Decode { value: String },
+    Info { torrent: PathBuf },
+}
+
+fn main() -> anyhow::Result<()> {
+    let args = Args::parse();
+    match args.command {
+        Command::Decode { value: _ } => {
+            todo!();
+        }
+        Command::Info { torrent } => {
+            let torrent_file = std::fs::read(torrent).context("read torrent file")?;
+            let t: Torrent =
+                serde_bencode::from_bytes(&torrent_file).context("parse torrent file")?;
+            println!("{:?}", t);
+        }
+    }
+    Ok(())
 }
